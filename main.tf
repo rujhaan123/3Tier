aadcc57346d1d00106d3bf244bb063f1342d16fc
Rujhaan Bhatia <33410430+rujhaan123@users.noreply.github.com>
2021-05-15 23:00:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,19 +21,15 @@ module "compute" {
   resource_group = module.resourcegroup.resource_group_name
   web_subnet_id = module.networking.websubnet_id
   app_subnet_id = module.networking.appsubnet_id
+  db_subnet_id = module.networking.dbsubnet_id
   web_host_name = var.web_host_name
   web_username = var.web_username
   web_os_password = var.web_os_password
   app_host_name = var.app_host_name
   app_username = var.app_username
   app_os_password = var.app_os_password
+  db_host_name = var.db_host_name
+  db_username = var.db_username
+  db_os_password = var.db_os_password
 }
 
-module "database" {
-  location = module.resourcegroup.location_id
-  resource_group = module.resourcegroup.resource_group_name
-  primary_database = var.primary_database
-  primary_database_version = var.primary_database_version
-  primary_database_admin = var.primary_database_admin
-  primary_database_password = var.primary_database_password
-}
